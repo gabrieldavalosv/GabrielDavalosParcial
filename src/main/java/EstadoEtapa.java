f4de8f0ad3478f87e99054280a42212010a5abf3
MathiMomos <mathi.gamer.m@gmail.com>
2024-10-07 12:00:25 -0500

    SIGUIENTE AVANCE

--- src/main/java/EstadoEtapa.java
+++ src/main/java/EstadoEtapa.java
@@ -1,4 +1,30 @@
 public class EstadoEtapa {
     private String nombre;
     private boolean estado;
+
+    public EstadoEtapa(String nombre, boolean estado) {
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public void habilitar() {
+        this.estado = true;
+    }
+
+    public void desabilitar() {
+        this.estado = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
 }
+
--- src/main/java/Etapa.java
+++ src/main/java/Etapa.java
@@ -3,4 +3,24 @@ import java.util.Date;
 public class Etapa {
     private Date fecha;
     private String glosa;
+
+    public Etapa(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getGlosa() {
+        return glosa;
+    }
+
+    public void setGlosa(String glosa) {
+        this.glosa = glosa;
+    }
 }
