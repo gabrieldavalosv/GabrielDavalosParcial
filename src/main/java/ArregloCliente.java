741750e88d072b293899b050f60374e31e751e11
MathiMomos <mathi.gamer.m@gmail.com>
2024-10-07 12:30:47 -0500

    INCORPORACION DE LOGICA DE METODOS CASI TERMINADO

--- src/main/java/ArregloCliente.java
+++ src/main/java/ArregloCliente.java
@@ -1,8 +1,41 @@
 public class ArregloCliente {
     private int indice;
+    private Cliente[] clientes;
 
-    public ArregloCliente(int indice) {
-        this.indice = indice;
+    public ArregloCliente() {
+        this.indice = 0;
+        this.clientes = new Cliente[10];
     }
+
+    public boolean agregar(Cliente cliente) {
+        if(indice < clientes.length) {
+            clientes[indice] = cliente;
+            indice++;
+            return true;
+        }
+        return false;
+    }
+
+    public Cliente buscarRUC(String RUC) {
+        for(int i = 0; i < indice; i++) {
+            if(clientes[i].getRUC().equals(RUC)) {
+                return clientes[i];
+            }
+        }
+        return null;
+    }
+
+    public Cliente[] buscarNombre(String nombre) {
+        Cliente[] resultados = new Cliente[indice];
+        int cont = 0;
+        for(int i = 0; i < indice; i++) {
+            if(clientes[i].getNombre().equals(nombre)) {
+                resultados[cont] = clientes[i];
+                cont++;
+            }
+        }
+        return resultados;
+    }
+
 
 }
--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -4,4 +4,94 @@ public class Cliente {
     private String email;
     private String telefono;
     private String clave;
+    private Oferta[] ofertas;
+    private Contacto contacto;
+    private int indiceOferta;
+
+    public Cliente(String RUC, String nombre, String email, String telefono, String clave) {
+        this.RUC = RUC;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.clave = clave;
+        this.ofertas = new Oferta[10];
+        this.indiceOferta = 0;
+    }
+
+    public boolean agregarOferta(Oferta oferta) {
+        if (indiceOferta < ofertas.length) {
+            ofertas[indiceOferta] = oferta;
+            indiceOferta++;
+            return true;
+        }
+        return false;
+    }
+
+    public void registrarContacto(String DNI, String nombres, String apellidos, String puesto) {
+        this.contacto = new Contacto(DNI, nombres, apellidos, puesto);
+    }
+
+    public Oferta[] getOfertas() {
+        return ofertas;
+    }
+
+    public String getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public void setOfertas(Oferta[] ofertas) {
+        this.ofertas = ofertas;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+
+    public int getIndiceOferta() {
+        return indiceOferta;
+    }
+
+    public void setIndiceOferta(int indiceOferta) {
+        this.indiceOferta = indiceOferta;
+    }
 }
